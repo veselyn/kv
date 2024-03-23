9183d73630e9360323f6e7c5a2b1310cb7fad2b4
Veselin Ivanov <me@veselyn.com>
2024-03-24 00:38:58 +0100

    build(flake): set up treefmt

--- flake.nix
+++ flake.nix
@@ -5,6 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils/main";
     devenv.url = "github:cachix/devenv/main";
+    treefmt.url = "github:numtide/treefmt-nix/main";
   };
 
   outputs = {
@@ -12,17 +13,35 @@
     nixpkgs,
     flake-utils,
     devenv,
+    treefmt,
   } @ inputs:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = import nixpkgs {inherit system;};
+      treefmtModule = treefmt.lib.evalModule pkgs ./treefmt.nix;
     in {
+      checks.ci = self.devShells.${system}.default.config.ciDerivation;
+      formatter = treefmtModule.config.build.wrapper;
+
       packages = {
         devenv-up = self.devShells.${system}.default.config.procfileScript;
       };
 
       devShells.default = devenv.lib.mkShell {
         inherit inputs pkgs;
-        modules = [];
+        modules = [
+          {
+            packages = [treefmtModule.config.build.wrapper];
+
+            pre-commit = {
+              hooks = {
+                treefmt.enable = true;
+              };
+              settings = {
+                treefmt.package = treefmtModule.config.build.wrapper;
+              };
+            };
+          }
+        ];
       };
     });
 }
---
+++ treefmt.nix
@@ -0,0 +1,7 @@
+{
+  projectRootFile = "flake.nix";
+
+  programs = {
+    alejandra.enable = true;
+  };
+}
