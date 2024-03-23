01875a91ecd58ac972f56e57a25fe458a9efaa0c
Veselin Ivanov <me@veselyn.com>
2024-03-23 23:40:35 +0100

    build(flake): specify outputs for each default system

--- flake.nix
+++ flake.nix
@@ -3,8 +3,14 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils/main";
   };
 
-  outputs = { self, nixpkgs }: {
-  };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: {
+    });
 }
