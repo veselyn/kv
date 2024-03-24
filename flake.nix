85ca1812322737d960b5fd1bdb2c11cfc9bf69a1
Veselin Ivanov <me@veselyn.com>
2024-03-24 01:25:59 +0100

    build(flake): set up devenv

--- flake.nix
+++ flake.nix
@@ -4,13 +4,25 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils/main";
+    devenv.url = "github:cachix/devenv/main";
   };
 
   outputs = {
     self,
     nixpkgs,
     flake-utils,
-  }:
-    flake-utils.lib.eachDefaultSystem (system: {
+    devenv,
+  } @ inputs:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
+      packages = {
+        devenv-up = self.devShells.${system}.default.config.procfileScript;
+      };
+
+      devShells.default = devenv.lib.mkShell {
+        inherit inputs pkgs;
+        modules = [];
+      };
     });
 }
