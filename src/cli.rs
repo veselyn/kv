46e9ca81df62cbdcb5ce32cd295ed4e068f80222
Veselin Ivanov <me@veselyn.com>
2024-06-06 21:33:28 +0200

    feat(cli): move main context into a run method

--- src/cli.rs
+++ src/cli.rs
@@ -1,4 +1,7 @@
+use anyhow::Context;
 use clap::{Parser, Subcommand};
+
+use crate::{app::App, migrations};
 
 #[derive(Parser, Debug)]
 pub struct Cli {
@@ -14,4 +17,33 @@ pub enum Command {
     Set { key: String, value: String },
     #[command(about = "Delete the key")]
     Del { key: String },
+}
+
+impl Cli {
+    pub fn run(self) -> anyhow::Result<()> {
+        let data_dir = dirs::data_dir().context("getting data directory")?;
+        let db_dir = data_dir.join("kv");
+        std::fs::create_dir_all(&db_dir)?;
+        let db_path = db_dir.join("db");
+
+        let mut db = rusqlite::Connection::open(db_path)?;
+        migrations::run(&mut db)?;
+
+        let app = App::new(db);
+
+        match self.command {
+            Command::Get { key } => {
+                let value = app.get(key)?;
+                println!("{}", value);
+            }
+            Command::Set { key, value } => {
+                app.set(key, value)?;
+            }
+            Command::Del { key } => {
+                app.del(key)?;
+            }
+        }
+
+        Ok(())
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,36 +2,11 @@ mod app;
 mod cli;
 mod migrations;
 
-use anyhow::Context;
-use app::App;
 use clap::Parser;
-use cli::{Cli, Command};
+use cli::Cli;
 
 fn main() -> anyhow::Result<()> {
     let cli = Cli::parse();
-
-    let data_dir = dirs::data_dir().context("getting data directory")?;
-    let db_dir = data_dir.join("kv");
-    std::fs::create_dir_all(&db_dir)?;
-    let db_path = db_dir.join("db");
-
-    let mut db = rusqlite::Connection::open(db_path)?;
-    migrations::run(&mut db)?;
-
-    let app = App::new(db);
-
-    match cli.command {
-        Command::Get { key } => {
-            let value = app.get(key)?;
-            println!("{}", value);
-        }
-        Command::Set { key, value } => {
-            app.set(key, value)?;
-        }
-        Command::Del { key } => {
-            app.del(key)?;
-        }
-    }
-
+    cli.run()?;
     Ok(())
 }
