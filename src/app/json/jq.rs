70a1b1cdb2a3129ea6fae2450560ac2dfc5fc0e8
Veselin Ivanov <me@veselyn.com>
2024-06-15 15:07:40 +0200

    feat(json): ensure memory is cleaned up after format

--- src/app/json/jq.rs
+++ src/app/json/jq.rs
@@ -4,34 +4,74 @@ where
 {
     let c_input = std::ffi::CString::new(input.into())?;
 
-    let formatted = unsafe {
+    let mut memstream = Memstream::open()?;
+
+    unsafe {
         let jv = jq_sys::jv_parse(c_input.as_ptr());
-
-        let mut buffer: *mut libc::c_char = std::ptr::null_mut();
-        let mut size: libc::size_t = 0;
-
-        let memstream = libc::open_memstream(
-            std::ptr::from_mut(&mut buffer),
-            std::ptr::from_mut(&mut size),
-        );
-        anyhow::ensure!(!memstream.is_null());
 
         jq_sys::jv_dumpf(
             jv,
-            memstream as *mut jq_sys::FILE,
+            memstream.file as *mut jq_sys::FILE,
             (jq_sys::jv_print_flags_JV_PRINT_PRETTY | jq_sys::jv_print_flags_JV_PRINT_SPACE2)
-                .try_into()?,
+                as i32,
         );
-
-        let status = libc::fclose(memstream);
-        anyhow::ensure!(status == 0);
-
-        let formatted = std::ffi::CStr::from_ptr(buffer).to_str()?.to_string();
-
-        libc::free(buffer as *mut libc::c_void);
-
-        formatted
     };
 
-    Ok(formatted)
+    let c_output = unsafe { std::ffi::CStr::from_ptr(memstream.buffer) };
+    let output = c_output.to_str().map(|str| str.to_string());
+
+    memstream.close()?;
+
+    Ok(output?)
+}
+
+#[derive(Debug)]
+struct Memstream {
+    buffer: *mut libc::c_char,
+    _size: libc::size_t,
+    file: *mut libc::FILE,
+    closed: bool,
+}
+
+impl Memstream {
+    fn open() -> anyhow::Result<Self> {
+        let mut buffer: *mut libc::c_char = std::ptr::null_mut();
+        let mut size: libc::size_t = 0;
+
+        let file = unsafe {
+            libc::open_memstream(
+                std::ptr::from_mut(&mut buffer),
+                std::ptr::from_mut(&mut size),
+            )
+        };
+        anyhow::ensure!(!file.is_null());
+
+        Ok(Self {
+            buffer,
+            _size: size,
+            file,
+            closed: false,
+        })
+    }
+
+    fn close(&mut self) -> anyhow::Result<()> {
+        if self.closed {
+            return Ok(());
+        }
+
+        let status = unsafe { libc::fclose(self.file) };
+        unsafe { libc::free(self.buffer as *mut libc::c_void) }
+
+        anyhow::ensure!(!status.is_positive());
+
+        self.closed = true;
+
+        Ok(())
+    }
+}
+
+impl Drop for Memstream {
+    fn drop(&mut self) {
+        assert!(self.closed, "memstream not closed");
+    }
 }
