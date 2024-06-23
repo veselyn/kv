ba52fb4f7b9389933a3edc9593d4377522bebdae
Veselin Ivanov <me@veselyn.com>
2024-06-23 20:13:43 +0200

    refactor(app): add some blank lines to init method

--- kv/src/app/mod.rs
+++ kv/src/app/mod.rs
@@ -13,8 +13,10 @@ impl App {
         env_logger::try_init()?;
 
         let data_dir = dirs::data_dir().ok_or(Error::GetDataDir)?;
+
         let db_dir = data_dir.join("kv");
         std::fs::create_dir_all(&db_dir).map_err(Error::CreateKvDir)?;
+
         let db_path = db_dir.join("db");
         std::fs::File::options()
             .create(true)
@@ -22,6 +24,7 @@ impl App {
             .append(true)
             .open(&db_path)
             .map_err(Error::CreateDbFile)?;
+
         let db_url = format!("sqlite://{}", db_path.display());
 
         let db = Database::connect_and_migrate(db_url).await?;
