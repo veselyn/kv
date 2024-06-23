a42169b0566ef195357951069e5a5e5e840edbb0
Veselin Ivanov <me@veselyn.com>
2024-06-23 20:12:03 +0200

    refactor(app): return concrete error type

--- kv/src/app/mod.rs
+++ kv/src/app/mod.rs
@@ -1,5 +1,7 @@
+mod error;
+
 use crate::{database::Database, json};
-use anyhow::Context;
+pub use error::*;
 
 #[derive(Debug)]
 pub struct App {
@@ -7,18 +9,19 @@ pub struct App {
 }
 
 impl App {
-    pub async fn init() -> anyhow::Result<Self> {
-        env_logger::init();
+    pub async fn init() -> Result<Self, Error> {
+        env_logger::try_init()?;
 
-        let data_dir = dirs::data_dir().context("getting data directory")?;
+        let data_dir = dirs::data_dir().ok_or(Error::GetDataDir)?;
         let db_dir = data_dir.join("kv");
-        std::fs::create_dir_all(&db_dir)?;
+        std::fs::create_dir_all(&db_dir).map_err(Error::CreateKvDir)?;
         let db_path = db_dir.join("db");
         std::fs::File::options()
             .create(true)
             .truncate(false)
             .append(true)
-            .open(&db_path)?;
+            .open(&db_path)
+            .map_err(Error::CreateDbFile)?;
         let db_url = format!("sqlite://{}", db_path.display());
 
         let db = Database::connect_and_migrate(db_url).await?;
