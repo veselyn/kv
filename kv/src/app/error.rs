01c9298caa029fe48e811fa9e1d4fe6f697a209d
Veselin Ivanov <me@veselyn.com>
2024-06-27 21:09:14 +0200

    refactor(app): rename init method to new

--- kv/src/app/error.rs
+++ kv/src/app/error.rs
@@ -3,7 +3,7 @@ use thiserror::Error;
 use crate::database;
 
 #[derive(Debug, Error)]
-pub enum InitError {
+pub enum Error {
     #[error("getting os specific data dir")]
     GetDataDir,
     #[error("creating kv dir in data dir: {0}")]
