fb5024753cc116111347ed2bfc78775266e690a8
Veselin Ivanov <me@veselyn.com>
2024-06-28 22:26:58 +0200

    feat(app): add config builder

--- kv/src/app/config.rs
+++ kv/src/app/config.rs
@@ -8,15 +8,35 @@ pub struct Config {
 
 impl Config {
     pub fn new() -> Result<Self, ConfigError> {
-        let data_dir = dirs::data_dir().ok_or(ConfigError::GetDataDir)?;
-        let db_dir = data_dir.join("kv");
-        let db_path = db_dir.join("db");
-
-        Ok(Self { db_path })
+        Self::builder().build()
     }
 
-    pub fn _db_path(mut self, db_path: PathBuf) -> Self {
-        self.db_path = db_path;
+    pub fn builder() -> Builder {
+        Builder::default()
+    }
+}
+
+#[derive(Debug, Default)]
+pub struct Builder {
+    pub db_path: Option<PathBuf>,
+}
+
+impl Builder {
+    pub fn build(self) -> Result<Config, ConfigError> {
+        let db_path = match self.db_path {
+            Some(db_path) => db_path,
+            None => {
+                let data_dir = dirs::data_dir().ok_or(ConfigError::GetDataDir)?;
+                let db_dir = data_dir.join("kv");
+                db_dir.join("db")
+            }
+        };
+
+        Ok(Config { db_path })
+    }
+
+    pub fn db_path(mut self, db_path: PathBuf) -> Self {
+        self.db_path = Some(db_path);
         self
     }
 }
