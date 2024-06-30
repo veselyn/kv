19970fdb4ca83d47ebf086458899ed792f12a21d
Veselin Ivanov <me@veselyn.com>
2024-06-30 17:30:11 +0200

    feat(json): colorize output with jq

--- kv/tests/json.rs
+++ kv/tests/json.rs
@@ -1,10 +1,12 @@
 use clap::Parser;
 use kv::{app::App, config::Config, Cli};
 use pretty_assertions::assert_eq;
+use std::io::Write;
+use std::process::{Command, Stdio};
 use tempfile::NamedTempFile;
 
 #[async_std::test]
-async fn formats_output() -> anyhow::Result<()> {
+async fn formats_output_with_jq() -> anyhow::Result<()> {
     let temp_file = NamedTempFile::new()?;
     let db_path = temp_file.path();
 
@@ -21,13 +23,26 @@ async fn formats_output() -> anyhow::Result<()> {
         .run_with(&app)
         .await?;
 
+    let mut jq = Command::new("jq")
+        .arg("--join-output")
+        .arg("--color-output")
+        .args(["--indent", "4"])
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .env("JQ_COLORS", "0;90:0;39:0;39:0;39:0;32:1;39:1;39:34;1")
+        .spawn()?;
+
+    jq.stdin
+        .as_mut()
+        .unwrap()
+        .write_all(br#"{"key":"value"}"#)?;
+
+    let output = jq.wait_with_output()?;
+    assert!(output.status.success());
+
     assert_eq!("", result.stderr);
-    assert_eq!(
-        r#"{
-    "key": "value"
-}"#,
-        result.stdout
-    );
+    std::assert_eq!(String::from_utf8(output.stdout)?, result.stdout);
 
     Ok(())
 }
