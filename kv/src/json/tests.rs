9f28e9c053b697fa62ec56f9624659007d481fd1
Veselin Ivanov <me@veselyn.com>
2024-07-27 13:29:01 +0300

    test(json): getting the value

--- kv/src/json/tests.rs
+++ kv/src/json/tests.rs
@@ -3,6 +3,70 @@ use anyhow::Result;
 use pretty_assertions::assert_eq;
 use serde_json::json;
 
+mod gets_the_value {
+    use super::*;
+
+    macro_rules! test {
+        ($name:ident, $key:expr, $initial:tt, $paths:expr, $want:tt) => {
+            #[async_std::test]
+            async fn $name() -> Result<()> {
+                let service = Service::default();
+                let key = $key;
+                let initial = json!($initial).to_string();
+                let paths: Option<&[&str]> = $paths;
+                let want = json!($want).to_string();
+                service.set(key, None, initial).await?;
+
+                let result = service.get(key, paths).await?;
+
+                self::assert_eq!(want, result);
+
+                Ok(())
+            }
+        };
+    }
+
+    test!(without_path, "key", { "a": 2, "c": [4, 5, { "f": 7 }] }, None, { "a": 2, "c": [4, 5, { "f": 7 }] });
+
+    mod single_path {
+        use super::*;
+
+        test!(root_path, "root_path", { "a": 2, "c": [4, 5, { "f": 7 }] }, Some(&["$"]), { "a": 2, "c": [4, 5, { "f": 7 }] });
+        test!(simple_key, "simple_key", { "a": 2, "c": [4, 5, { "f": 7 }] }, Some(&["$.c"]), [4, 5, { "f": 7 }]);
+        test!(simple_key_without_dollar, "simple_key_without_dollar", { "a": 2, "c": [4, 5, { "f": 7 }] }, Some(&["c"]), [4, 5, { "f": 7 }]);
+        test!(array_index, "array_index", { "a": 2, "c": [4, 5, { "f": 7 }] }, Some(&["$.c[2]"]), { "f": 7 });
+        test!(array_index_with_key, "array_index_with_key", { "a": 2, "c": [4, 5, { "f": 7 }] }, Some(&["$.c[2].f"]), 7);
+        test!(last_array_index, "last_array_index", { "a": 2, "c": [4, 5], "f": 7 }, Some(&["$.c[#-1]"]), 5);
+        test!(
+            bool,
+            "bool",
+            [false, false, false, true],
+            Some(&["3"]),
+            true
+        );
+        test!(number, "number", [11, 22, 33, 44], Some(&["3"]), 44);
+        test!(string, "string", { "a": "xyz" }, Some(&["$.a"]), "xyz");
+        test!(null, "null", { "a": null }, Some(&["$.a"]), null);
+    }
+
+    mod multiple_paths {
+        use super::*;
+
+        test!(roots, "multiple_roots", { "a": 2, "c": [4, 5, { "f": 7 }] }, Some(&["$", "$"]), { "$": { "a": 2, "c": [4, 5, { "f": 7 }] } });
+
+        test!(simple_keys, "simple_keys", { "a": 2, "c": [4, 5, { "f": 7 }] }, Some(&["$.a", "$.c"]), { "$.a": 2, "$.c": [4, 5, { "f": 7 }] });
+        test!(simple_keys_without_dollar, "simple_keys_without_dollar", { "a": 2, "c": [4, 5, { "f": 7 }] }, Some(&["a", "c"]), { "a": 2, "c": [4, 5, { "f": 7 }] });
+        test!(simple_keys_with_and_without_dollar, "simple_keys_with_and_without_dollar", { "a": 2, "c": [4, 5, { "f": 7 }] }, Some(&["$.a", "a", "c"]), { "$.a": 2, "a": 2, "c": [4, 5, { "f": 7 }] });
+        test!(array_indexes, "array_indexes", { "a": 2, "c": [4, 5, { "f": 7 }] }, Some(&["$.c[1]", "$.c[2]"]), { "$.c[1]": 5, "$.c[2]": { "f": 7 } });
+        test!(array_indexes_with_key, "array_indexes_with_key", { "a": 2, "c": [4, { "b": 5 }, { "f": 7 }] }, Some(&["$.c[1].b", "$.c[2].f"]), { "$.c[1].b": 5, "$.c[2].f": 7 });
+        test!(last_array_indexes, "last_array_indexes", { "a": 2, "c": [4, 5], "f": 7 }, Some(&["$.c[#-2]", "$.c[#-1]"]), { "$.c[#-2]": 4, "$.c[#-1]": 5 });
+        test!(bools, "bools", [false, false, false, true], Some(&["2", "3"]), { "2": false, "3": true });
+        test!(numbers, "numbers", [11, 22, 33, 44], Some(&["2", "3"]), { "2": 33, "3": 44 });
+        test!(strings, "strings", { "a": "xyz", "b": "abc" }, Some(&["$.a", "$.b"]), { "$.a": "xyz", "$.b": "abc" });
+        test!(nulls, "nulls", { "a": null, "b": null }, Some(&["$.a", "$.b"]), { "$.a": null, "$.b": null });
+    }
+}
+
 mod sets_the_value {
     use super::*;
 
@@ -181,10 +245,10 @@ mod sets_the_value_fail {
         };
     }
 
-    test!(key_not_found, "key", Some("$.key"), json!("value").to_string(), Err(SetError::KeyNotFound(key)) if key == "key");
+    test!(key_not_found, "key_not_found", Some("$.key"), json!("value").to_string(), Err(SetError::KeyNotFound(key)) if key == "key_not_found");
     test!(
         malformed_json,
-        "key",
+        "malformed_json",
         None,
         "invalid",
         Err(SetError::InvalidJson(_))
