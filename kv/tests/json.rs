3f8f21971f7eb01c32b6227be67c1a2381ca0b99
Veselin Ivanov <me@veselyn.com>
2024-08-04 20:13:49 +0200

    test(json): extract jq subprocess to fn

--- kv/tests/json.rs
+++ kv/tests/json.rs
@@ -4,6 +4,28 @@ use common::*;
 use std::io::Write;
 use std::process::{self, Stdio};
 
+fn jq(value: &str) -> String {
+    let mut jq_child = process::Command::new("jq")
+        .args(["--indent", "4"])
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .spawn()
+        .unwrap();
+
+    jq_child
+        .stdin
+        .take()
+        .unwrap()
+        .write_all(value.as_bytes())
+        .unwrap();
+
+    let jq_output = jq_child.wait_with_output().unwrap();
+    assert!(jq_output.status.success());
+
+    String::from_utf8(jq_output.stdout).unwrap()
+}
+
 #[test]
 fn gets_sets_and_deletes_key() {
     let cmd = Cli::new().to_cmd();
@@ -48,24 +70,6 @@ fn gets_sets_and_deletes_key() {
 fn replicates_jq_output() {
     let value = r#"{"key":"value"}"#;
 
-    let mut jq_child = process::Command::new("jq")
-        .args(["--indent", "4"])
-        .stdin(Stdio::piped())
-        .stdout(Stdio::piped())
-        .stderr(Stdio::piped())
-        .spawn()
-        .unwrap();
-
-    jq_child
-        .stdin
-        .take()
-        .unwrap()
-        .write_all(value.as_bytes())
-        .unwrap();
-
-    let jq_output = jq_child.wait_with_output().unwrap();
-    assert!(jq_output.status.success());
-
     let cmd = Cli::new().to_cmd();
 
     cmd()
@@ -79,6 +83,6 @@ fn replicates_jq_output() {
         .args(["json", "get", "key"])
         .assert()
         .success()
-        .stdout(String::from_utf8(jq_output.stdout).unwrap())
+        .stdout(jq(value))
         .stderr("");
 }
